24c73dff9d2953a830614951abf40516122194d1
Marcos Leite <m.abner@live.co.uk>
2023-12-27 17:38:34 -0300

    polymorphism example #2

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -8,5 +8,10 @@ namespace Herança_e_Polimorfismo_com_CSharp.Models
     public class Professor : Pessoa
     {
         public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, sou professor e ganho {Salario}.");
+        }
     }
 }
